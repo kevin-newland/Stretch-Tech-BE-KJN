e882a88615cc6da5790283dd71ad9933caad562b
Kevin Newland <kevin.newland@yahoo.com>
2025-04-29 11:34:25 -0500

    seeded db with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.destroy_all
+
+require "faker"
+
+10.times do
+  User.create!(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: "password",
+  )
+end
